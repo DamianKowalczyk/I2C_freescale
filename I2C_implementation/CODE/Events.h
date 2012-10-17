8437297e758d63dcfbaf26e1dc000717f02e45a1
Damian <damian13kowalczyk@gmail.com>
2012-10-17 16:02:51 +0200

    basic settings for CFV1 project

---
+++ I2C_implementation/CODE/Events.h
@@ -0,0 +1,38 @@
+/** ###################################################################
+**     Filename  : Events.H
+**     Project   : I2C_implementation
+**     Processor : MCF51AC256ACLK
+**     Component : Events
+**     Version   : Driver 01.02
+**     Compiler  : CodeWarrior ColdFireV1 C Compiler
+**     Date/Time : 2012-10-17, 16:00
+**     Abstract  :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings  :
+**     Contents  :
+**         No public methods
+**
+** ###################################################################*/
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 3.07 [04.34]
+**     for the Freescale ColdFireV1 series of microcontrollers.
+**
+** ###################################################################
+*/
