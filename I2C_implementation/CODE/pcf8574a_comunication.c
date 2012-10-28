2dcf23d56e04c070dfec5610f0fbb42bcdd2e0e3
Damian <damian13kowalczyk@gmail.com>
2012-10-28 20:33:40 +0100

    deleted original function to get data from expander

--- I2C_implementation/CODE/pcf8574a_comunication.c
+++ I2C_implementation/CODE/pcf8574a_comunication.c
@@ -32,56 +32,6 @@ int sendSampleDataToExpander(char number)
 } 
 
 byte getSampleDataFromExpander(char* data)
-{    
-  int delay = 65536;
-          
-  clearInteruptFlag();
-  
-   // check if it is in transmit mode
-    // this fragment was changed
-  if(getRegBit(IICC1, TX)==0)
-    setRegBit(IICC1, TX);
-    
-  I2C_SendStart();     
-    
-  sendByteOfData(0b01111111); // inform expander that i will get some data from him
-  
-  while(getRegBit(IICS, IICIF) == 0 && delay != 0)  // wait for copletly sent byte
-    delay--;
-    
-  if (delay==0)  // if the byte was not correct send
-    return 7;
-  
-  clearInteruptFlag();
-    
-  delay = 65535;  
-  
-  while(getRegBit(IICS, RXAK) && delay != 0)  // wait for ack
-    delay--;
-    
-  if (delay==0)  // end if no ack get
-    return 5;
-  
-  clrRegBit(IICC1, TX); // receive information from device 
-    
-  setRegBit(IICC1, TXAK);  // No acknowledge signal response is sent
-  
-  *data = getReg(IICD);
-  
-  while(getRegBit(IICS, IICIF) == 0 && delay != 0)  // wait for copletly sent byte
-    delay--;
-    
-  if (delay==0)  // if the byte was not correct send
-    return 7;
-  
-  clearInteruptFlag();
-  
-  I2C_SendStop();
-  
-  return 1;   
-}
-
-byte getSampleDataFromExpander2(char* data)
 {           
   byte result = 15;
       
