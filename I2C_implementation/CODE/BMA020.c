b4fda2ceede6b20106053ab4131b95325f195c8d
Damian <damian13kowalczyk@gmail.com>
2012-11-04 17:51:56 +0100

    makes some modification into this module. Implement function to cornverting data from device

--- I2C_implementation/CODE/BMA020.c
+++ I2C_implementation/CODE/BMA020.c
@@ -14,22 +14,26 @@
 #include "PE_Types.h"
 #include "BMA020.h"
 
-byte BMA020_readRegistry(byte reg_addr); 
+byte BMA020_readRegistry(byte reg_addr);
+bool BMA020_checkSensor(); 
 
 struct bma020_str bma020;
 
-/** function for initialize the BMA020 device for using it in the future here we should set the registries of device to 
-    get correct values */
-void BMA020_init()
+/** function reads necessary parameters from device and checks if device works correctly 
+*/
+bool BMA020_init()
 {
-  bma020.reservedBitsFromRegistry_0x14 = readRegistry(0x14) & 11100000;
-  bma020.accelerationMeasurementRange = 0;
-  bma020.bandwidth = 0; 
+  bma020.reservedBitsFromRegistry_0x14 = BMA020_readRegistry(0x14) & 11100000;
+  bma020.accelerationMeasurementRange = 0; // default values after
+  bma020.filterBandwidth = 0;       // power on are 0
+  bma020.coefficient = RANGE_COEF_2G;
+  bma020.divider = RANGE_COEF_4G;  
+  bma020.base_value =RANGE_COEF_8G;
   
-  BMA020_checkSensor(); 
+  return BMA020_checkSensor(); 
 }
 
-void BMA020_readAccelerationValues(char* value_of_bytes)
+void BMA020_readAccelerationValues(char* bytes_val)
 {
   I2C_SendStart();
   I2C_SendByte_Ack(BMA020_WR_ADDR);
@@ -38,8 +42,37 @@ void BMA020_readAccelerationValues(char* value_of_bytes)
   
   I2C_SendStart();
   I2C_SendByte_Ack(BMA020_RD_ADDR);
-  I2C_Receive_N_Bytes(value_of_bytes, 6);
+  I2C_Receive_N_Bytes(bytes_val, 6);
   I2C_SendStop(); 
+}
+
+void BMA020_convertAccelerationValues(char* bytes_val, short* acceler_val)
+{
+  byte i, j;
+  unsigned long int tmp_result;
+  unsigned short tmp_val_lsb;
+  unsigned short tmp_val_msb;
+  unsigned short sign;
+  
+  for (i=0, j=0; i<6; i++, j+=2)
+  {
+    tmp_val_lsb = (*(bytes_val + j)) >> 6;    
+    tmp_val_msb = *(bytes_val + j +1) & 0b01111111;
+    tmp_val_msb <<= 2;
+    sign = *(bytes_val + j +1) & 0b10000000;
+    
+    tmp_result = tmp_val_lsb | tmp_val_msb;
+    tmp_result = (tmp_result * bma020.coefficient)/ bma020.divider; 
+    
+    if (sign) //if the negative number
+    {
+      *(acceler_val+i) = bma020.base_value + (short) tmp_result;    
+    } 
+    else 
+    {
+      *(acceler_val+i) = (short) tmp_result;    
+    }
+  }   
 }
   
 
@@ -65,15 +98,14 @@ void BMA020_setRegistry(byte reg_addr, byte value)
 {
   if (reg_addr == 0x14)
   {
-    value = (value & 00011111) | reservedBitsFromRegistry_0x14; // Bits 5, 6 and 7 of register addresses 14h do contain 
+    value = (value & 0b00011111) | bma020.reservedBitsFromRegistry_0x14; // Bits 5, 6 and 7 of register addresses 14h do contain 
                       //  critical sensor individual calibration data which must not be changed or deleted by any means.                                                                  
   } 
   else if (reg_addr == 0x0A) 
   {
-    value &= 01001111;    //Bit 7, bit 5 and bit 4 of register 0Ah should be left at a value of '0'
-  }
-  
-  // i2c comunication
+    value &= 0b01001111;    //Bit 7, bit 5 and bit 4 of register 0Ah should be left at a value of '0'
+  }           
+            // i2c comunication
   I2C_SendStart();
   I2C_SendByte_Ack(BMA020_WR_ADDR);
   I2C_SendByte_Ack(reg_addr);
@@ -83,20 +115,36 @@ void BMA020_setRegistry(byte reg_addr, byte value)
 
 
 
-/** function sets the range of measurement accleration, and the bandwidth of digital filters 
-    val_ran  range             val_band  bandwidth (Hz) 
-    0    ->   +/- 2g            0    ->   25
-    1    ->   +/- 4g            1    ->   50
-    2    ->   +/- 8g            2    ->   100
-                                3    ->   190
-                                4    ->   375
-                                5    ->   750
-                                6    ->   1500
-*/
-void BMA020_setRangeAndBandwidth(byte val_ran, byte val_band){
-  val_ran = (val_ran<<3) | val_band;
+/** function sets the range of measurement accleration 
+   val_ran   range              
+    0    ->   +/- 2g            
+    1    ->   +/- 4g            
+    2    ->   +/- 8g */
+void BMA020_setRangeAndBandwidth(byte val_ran)
+{
+  bma020.accelerationMeasurementRange = val_ran;
   
-  BMA020_setRegistry(0x14, (val_ran));  
+  val_ran = val_ran<<3 | bma020.filterBandwidth;
+  
+  BMA020_setRegistry(0x14, val_ran);  
+}
+
+/** function sets the bandwidth of digital filters
+val_band  bandwidth (Hz) 
+  0    ->   25
+  1    ->   50
+  2    ->   100
+  3    ->   190
+  4    ->   375
+  5    ->   750
+  6    ->   1500 */
+void BMA020_setBandwidth(byte val_band)
+{
+  bma020.filterBandwidth = val_band;
+  
+  val_band = bma020.accelerationMeasurementRange <<3 | val_band;
+  
+  BMA020_setRegistry(0x14, val_band);  
 }
 
 
@@ -110,7 +158,7 @@ bool BMA020_checkSensor()
   
   do {
     waitForEndingMeasurement();  // in future time i should do something better than this
-    endSelfTestFlag = BMA020_readRegistry(0x0A) & 00000100;
+    endSelfTestFlag = BMA020_readRegistry(0x0A) & 0b00000100;
   } while (endSelfTestFlag != 0);  
   
   result = BMA020_readRegistry(0x09);
--- I2C_implementation/CODE/BMA020.h
+++ I2C_implementation/CODE/BMA020.h
@@ -13,19 +13,35 @@
 #define __BMA020_H
 
 /* MODULE BMA020 */
-#define BMA020_WR_ADDR        0x70      
-#define BMA020_RD_ADDR        0x71
+#define BMA020_WR_ADDR           0x70      
+#define BMA020_RD_ADDR           0x71 
+
+#define RANGE_COEF_2G            3906
+#define RANGE_COEF_4G            7812
+#define RANGE_COEF_8G           15625
+
+#define RANGE_DIVIDER_2G         1000
+#define RANGE_DIVIDER_4G         1000
+#define RANGE_DIVIDER_8G        10000
+
+#define RANGE_BASE_2G           -2000
+#define RANGE_BASE_4G           -4000
+#define RANGE_BASE_8G           -8000
 
 struct bma020_str 
 {
   char reservedBitsFromRegistry_0x14;
   char accelerationMeasurementRange;
-  char bandwidth;
+  char filterBandwidth;
+  
+  unsigned long int coefficient;
+  unsigned long int divider;
+  short base_value;
 };
 
 
 
-/* END BMA020 */
+/* END BMA020 */        
 
 #endif
 
